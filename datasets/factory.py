14c12836c88673c59b1bf2b3ff36860e58eefa85
Remi Cadene <rcadene@lnode-au16-28.cm.cluster>
2018-07-14 16:47:27 -0700

    Improve compatibility with bootstrap v0.0.9

--- datasets/factory.py
+++ datasets/factory.py
@@ -1,17 +1,27 @@
 from bootstrap.lib.options import Options
 from .mnist import MNIST
 
-def factory(split):
+def factory(engine=None):
+    dataset = {}
 
     if Options()['dataset']['name'] == 'mnist':
-        dataset = MNIST(
-            dir_data=Options()['dataset']['dir'],
-            split=split,
-            batch_size=Options()['dataset']['batch_size'],
-            nb_threads=Options()['dataset']['nb_threads'],
-            pin_memory=Options()['misc']['cuda'])
+        if Options()['dataset']['train_split']:
+            dataset['train'] = factory_mnist(Options()['dataset']['train_split'])
+
+        if Options()['dataset']['eval_split']:
+            dataset['eval'] = factory_mnist(Options()['dataset']['eval_split'])
     else:
         raise ValueError()
 
+    return dataset
+
+
+def factory_mnist(split):
+    dataset = MNIST(
+        dir_data=Options()['dataset']['dir'],
+        split=split,
+        batch_size=Options()['dataset']['batch_size'],
+        nb_threads=Options()['dataset']['nb_threads'],
+        pin_memory=Options()['misc']['cuda'])
     return dataset
 
