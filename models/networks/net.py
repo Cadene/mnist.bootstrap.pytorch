32cd0690e5ac97e0f68782fba1c84df0037d1f27
Remi Cadene <rcadene@lnode-au16-28.cm.cluster>
2018-07-14 16:47:48 -0700

    Replace default model by simple

--- models/networks/net.py
+++ models/networks/net.py
@@ -11,8 +11,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, batch):
-        x = batch['data']
+    def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
