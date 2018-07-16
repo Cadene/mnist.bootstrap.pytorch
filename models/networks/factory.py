8b5bdb186b5ca2d0634b0397601c096b487169c0
Remi Cadene <rcadene@gnode-au11-01.cm.cluster>
2018-07-16 14:13:35 -0700

    Add multi gpus support

--- models/networks/factory.py
+++ models/networks/factory.py
@@ -1,5 +1,9 @@
+import torch
+import torch.nn as nn
+import bootstrap.lib.utils as utils
 from bootstrap.lib.options import Options
 from bootstrap.lib.logger import Logger
+from bootstrap.models.networks.data_parallel import DataParallel
 
 from .net import Net
 
@@ -9,6 +13,9 @@ def factory(engine=None):
 
     if Options()['model']['network']['name'] == 'net':
         network = Net()
+
+        if Options()['misc']['cuda'] and len(utils.available_gpu_ids()) >= 2:
+            network = DataParallel(network)
 
     else:
         raise ValueError()
