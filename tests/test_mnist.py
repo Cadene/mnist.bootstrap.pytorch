baed0a95a7c7c5ff5ea66d9392f5ef59c363b04b
Remi Cadene <remi.cadene@icloud.com>
2018-10-01 19:56:56 -0700

    Add tests

---
+++ tests/test_mnist.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from bootstrap.run import run
+from bootstrap.run import main
+from bootstrap.lib.options import Options
+
+
+def reset_options_instance():
+    Options._Options__instance = None
+    sys.argv = [sys.argv[0]] # reset command line args
+
+def test_run():
+    reset_options_instance()
+    sys.argv += [
+        '-o', 'mnist/options/sgd.yaml',
+        '--engine.nb_epochs', '1',
+        '--engine.debug', 'True'
+    ]
+    main(run=run)
