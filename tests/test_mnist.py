a5624be2ee727c9ccd81ce3fb11676301b4b6ef2
Remi Cadene <remi.cadene@icloud.com>
2018-10-01 23:28:24 -0700

    Add test_run (debug=False)

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -9,12 +9,21 @@ def reset_options_instance():
     Options._Options__instance = None
     sys.argv = [sys.argv[0]] # reset command line args
 
-def test_run():
+def test_run_debug():
     reset_options_instance()
     sys.argv += [
         '-o', 'mnist/options/sgd.yaml',
         '--engine.nb_epochs', '1',
         '--engine.debug', 'True',
+        '--exp.dir', 'logs/mnist/test_run_debug,True'
+    ]
+    main(run=run)
+
+def test_run():
+    reset_options_instance()
+    sys.argv += [
+        '-o', 'mnist/options/sgd.yaml',
+        '--engine.nb_epochs', '2',
         '--exp.dir', 'logs/mnist/test_run'
     ]
     main(run=run)
